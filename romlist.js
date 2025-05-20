1d0dfaeafe43ca9c33ee7c792deea59cb18dd888
Jordan <39-12602@sae.edu.za>
2025-05-20 08:53:18 +0200

    add roms

--- romlist.js
+++ romlist.js
@@ -1,5 +1,7 @@
 var ROMLIST = [
 
-    {url:"roms/Zelda.z64",title:"Zelda"},
+    {url:"roms/Zelda.z64",title:"Legend of Zelda"},
+    {url:"roms/MK64.z64",title:"Mario Kart 64 Amped Up"},
+    {url:"roms/Pikachu.z64",title:"Hey You, Pikachu"},
 
 ];
